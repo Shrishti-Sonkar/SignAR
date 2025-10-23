7d9d00e95b71ac358ed3bf772d64a7c3eadacf1f
Shrishti Sonkar <shrishtisonkar195@gmail.com>
2025-10-23 19:14:41 +0530

    Update detect_signs.py

--- detect_signs.py
+++ detect_signs.py
@@ -3,7 +3,6 @@ import cv2
 import numpy as np
 import pyttsx3
 from tensorflow.keras.models import load_model
-
 # ✅ Suppress TensorFlow warnings & logs
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -95,3 +94,4 @@ while True:
 # ✅ Release resources
 cap.release()
 cv2.destroyAllWindows()
+
